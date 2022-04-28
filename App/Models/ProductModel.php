8566852f2b592f0e2d83aa7686399140357afc7c
Henrique Silva <henriquensf2@gmail.coma>
2022-04-28 20:33:55 -0300

    insert and remove now depend on getters

--- App/Models/ProductModel.php
+++ App/Models/ProductModel.php
@@ -54,15 +54,15 @@
 
       $sql = 'INSERT INTO '.self::$table .'(sku, name, value, weight, height, width, length, size, type, created_at, updated_at) VALUES (:sku, :name, :value, :weight, :height, :width, :length, :size, :type, :created_at, :updated_at)';
       $stmt = $db->prepare($sql);
-      $stmt->bindValue(':sku', $product['sku']);
-      $stmt->bindValue(':name', $product['name']);
-      $stmt->bindValue(':value', $product['value']);
-      $stmt->bindValue(':weight', $product['weight']);
-      $stmt->bindValue(':height', $product['height']);
-      $stmt->bindValue(':width', $product['width']);
-      $stmt->bindValue(':length', $product['length']);
-      $stmt->bindValue(':size', $product['size']);
-      $stmt->bindValue(':type', $product['type']);
+      $stmt->bindValue(':sku', $product->getSku());
+      $stmt->bindValue(':name', $product->getName());
+      $stmt->bindValue(':value', $product->getValue());
+      $stmt->bindValue(':weight', $product->getWeight());
+      $stmt->bindValue(':height', $product->getHeight());
+      $stmt->bindValue(':width', $product->getWidth());
+      $stmt->bindValue(':length', $product->getLength());
+      $stmt->bindValue(':size', $product->getSize());
+      $stmt->bindValue(':type', $product->getType());
       $stmt->bindValue(':created_at', $current_time);
       $stmt->bindValue(':updated_at', $current_time);
     
@@ -73,12 +73,12 @@
         throw new \Exception("Failed to insert the product!");
       }
     }
-    public static function delete(string $sku){
+    public static function delete($product){
       $database = new Database;
       $db = $database->connect();
       $sql = 'DELETE FROM '.self::$table .' WHERE sku=:sku';
       $stmt = $db->prepare($sql);
-      $stmt->bindValue(':sku', $sku);
+      $stmt->bindValue(':sku', $product->getSku());
       $stmt->execute();
 
       if ($stmt->rowCount() > 0){
